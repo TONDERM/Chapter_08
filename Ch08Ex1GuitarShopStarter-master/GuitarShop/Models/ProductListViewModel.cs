70bfe0f1d029d223ad69838f3a6414c914e4634b
TONDERM <tonderm@mail.nmc.edu>
2022-11-14 18:26:26 -0500

    Add project files.

---
+++ Ch08Ex1GuitarShopStarter-master/GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GuitarShop.Models;
+
+namespace GuitarShop.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public object products { get; internal set; }
+        public string SelectedCategory { get; set; }
+        public string CheckActiveCategory(string category) => category == SelectedCategory ? "active" : "";
+    }
+}
